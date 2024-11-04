ab4fc552e5b17380c10a4f3f166eccdcbfb5f96e
senagab <senagab@gmail.com>
2024-11-04 17:16:16 -0400

    feat: configuração inicial, nova estrutura

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <script setup>
+const nome = "gabriel"
+const meuObjeto = {
+  nome: "gian",
+  filmeFavorito: "Dazed and Confused"
+}
+
+function dizOla(nome) {
+  return `${nome} diz oi`;
+}
+
+const enderecoDaImagem = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTepEI3ooX-xTWxqducQ90dHkaN0rJdUcFWpyoUfrGJPVv4PkNzvpfsK2trZy1MPN4KaAo&usqp=CAU"
+
+const botaoEstaDesabilitado = false
 </script>
 
 <template>
-  <h1>Olá</h1>
+  <h1>{{ dizOla("paula") }}</h1>
+  <img :src="enderecoDaImagem" alt="">
+
+  <button :disabled="!botaoEstaHabilitado">enviar mensagem</button>
 </template>
 
 <style scoped>
